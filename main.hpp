a45d19602baafb23e9c1e0321c7de8fa22aa0c9e
Angry_Programist <Kirill_071@bk.ru>
2020-12-07 23:01:13 +0300

    Добавил все функции в main.hpp

--- main.hpp
+++ main.hpp
@@ -1,10 +1,9 @@
+#pragma once
 #include <iostream>
 #include <string>
-#include "main.hpp"
 
 using namespace std;
 
-/*
 int CurrentRobbers = 0;
 int CurrentLiveRobbers = 0;
 
@@ -237,86 +236,4 @@ void menu(){
     cout << " 5 - to kill a robber " << endl;
     cout << " 6 - to get info about robber " << endl << endl;
 
-}
-
-
-*/
-
-int main() {
-    
-    Robber arr[40]; // спросить про размер массива структур
-
-
-    for (int i = 0; i<1; i++){ //пока только 1 разбойник имеется
-
-        cout << " Enter Robber " << i+1 << ":" << endl;
-        cin >> arr[i].name >> arr[i].life >> arr[i].skakun >> arr[i].sabli >> arr[i].rubin >> arr[i].ozher >> arr[i].wifes >> arr[i].money;
-
-        CurrentRobbers++;
-
-        if (arr[i].life) {
-            
-            CurrentLiveRobbers++;
-        }
-        cout << '\n';
-
-    }
-
-    char c;
-
-    do {
-
-        menu();
-
-        cin>>c;
-        cout<<'\n';
-
-        switch(c){
-
-            case '0':
-
-                break;
-
-            case '1':
-
-                PrintPobbers(arr);
-                break;
-
-            case '2':
-
-                PushRobber(arr);
-                break;
-
-            case '3': 
-
-                RobbersWealth(arr);
-                break;
-
-            case '4':
-            
-                RobbersProperty(arr);
-                break;
-
-            case '5': 
-
-                KillRobber(arr);
-                break;
-
-            case '6':
-
-                RobberInfo(arr);
-                break;
-
-            default:
-
-                cout << " incorrect data " << endl;
-                break;
-
-        }
-
-    }
-    while (c !='0');
-
-
-    return 0;
 }
